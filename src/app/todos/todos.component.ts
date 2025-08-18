905f3dacfb4805f7c2f7520f93703059613cfc0c
Marcelo Guerrero <m.guerrero@mpbit.cl>
2025-08-17 22:31:02 -0400

    Add AWS Amplify UI Angular and update dependencies
    Integrated @aws-amplify/ui-angular and its theme into the Angular project. Updated several AWS Amplify-related dependencies to their latest versions in package.json and package-lock.json. Modified angular.json to include the Amplify UI theme CSS.

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -6,39 +6,43 @@ import type { Schema } from '../../../amplify/data/resource';
 const client = generateClient<Schema>();
 
 @Component({
-  selector: 'app-todos',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css',
+    selector: 'app-todos',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './todos.component.html',
+    styleUrl: './todos.component.css',
 })
 export class TodosComponent implements OnInit {
-  todos: any[] = [];
+    todos: any[] = [];
 
-  ngOnInit(): void {
-    this.listTodos();
-  }
-
-  listTodos() {
-    try {
-      client.models.Todo.observeQuery().subscribe({
-        next: ({ items, isSynced }) => {
-          this.todos = items;
-        },
-      });
-    } catch (error) {
-      console.error('error fetching todos', error);
+    ngOnInit(): void {
+        this.listTodos();
     }
-  }
 
-  createTodo() {
-    try {
-      client.models.Todo.create({
-        content: window.prompt('Todo content'),
-      });
-      this.listTodos();
-    } catch (error) {
-      console.error('error creating todos', error);
+    listTodos() {
+        try {
+            client.models.Todo.observeQuery().subscribe({
+                next: ({ items, isSynced }) => {
+                    this.todos = items;
+                },
+            });
+        } catch (error) {
+            console.error('error fetching todos', error);
+        }
     }
-  }
+
+    createTodo() {
+        try {
+            client.models.Todo.create({
+                content: window.prompt('Todo content'),
+            });
+            this.listTodos();
+        } catch (error) {
+            console.error('error creating todos', error);
+        }
+    }
+
+    deleteTodo(id: string) {
+        client.models.Todo.delete({ id })
+    }
 }
